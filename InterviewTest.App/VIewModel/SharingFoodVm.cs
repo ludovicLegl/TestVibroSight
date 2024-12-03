bc1d7503eda990771a5cd48aaf5917d5389f1b94
ludovic <ludovic.legland@protonmail.ch>
2024-12-03 11:09:17 +0100

    Add ProductList Repaire CheckAvailability Improve code

--- InterviewTest.App/VIewModel/SharingFoodVm.cs
+++ InterviewTest.App/VIewModel/SharingFoodVm.cs
@@ -2,12 +2,11 @@
 using InterviewTest.App.Service;
 using InterviewTest.App.ViewModel.Helper;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
@@ -17,10 +16,25 @@ namespace InterviewTest.App.ViewModel
     public class SharingFoodVm : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-        private readonly List<IProduct> _products = [];
-        private readonly IProductStore _productStore;
+        public ObservableCollection<IProduct> Products { get; set; }
+
         public ObservableCollection<string> Types { get; set; }
+
+        private readonly IProductStore _productStore;
+
         public ICommand AddProductCommand { get; }
+        public ICommand CheckAvailabilityCommand { get; }
+
+        private bool _isCheckingAvailability;
+        public bool IsCheckingAvailability 
+        {
+            get => _isCheckingAvailability;
+            set
+            {
+                _isCheckingAvailability = value;
+                OnPropertyChanged(nameof(IsCheckingAvailability));
+            }
+        }
 
         private string _name;
         public string Name
@@ -69,88 +83,142 @@ namespace InterviewTest.App.ViewModel
                 OnPropertyChanged(nameof(SelectedType));
             }
         }
+        private ObservableCollection<string> GetProductTypes()
+        {
+            var productTypes = Assembly.GetExecutingAssembly()
+                                       .GetTypes()
+                                       .Where(t => t.IsClass && !t.IsAbstract && typeof(IProduct).IsAssignableFrom(t))
+                                       .Select(t => t.Name)
+                                       .ToList();
 
-
-
-
+            return new ObservableCollection<string>(productTypes);
+        }
 
         public SharingFoodVm()
         {
+            //TODO could Improve this
             _productStore = ServiceProvider.Instance.ProductStore;
-            //TODO
-            _products.AddRange(_productStore.GetProducts());
+            Products = new ObservableCollection<IProduct>(_productStore.GetProducts());
             _productStore.ProductAdded += _productStore_ProductAdded;
             _productStore.ProductRemoved += _productStore_ProductRemoved;
-            Types = new ObservableCollection<string> { "Fruit", "Vegetable" };
 
+            Types = GetProductTypes();
+            IsCheckingAvailability = true;
 
             AddProductCommand = new AsyncRelayCommand(AddProduct);
+            CheckAvailabilityCommand = new AsyncRelayCommand(CheckAvailability);
         }
 
-        private void _productStore_ProductAdded(IProduct obj)
+        private void _productStore_ProductAdded(IProduct product)
         {
-            _products.Add(obj);
-            RefreshProducts();
+            UpdateProductsCollection(() => Products.Add(product));
         }
-        private void _productStore_ProductRemoved(Guid obj)
+
+        private void _productStore_ProductRemoved(Guid idProduct)
         {
-            IProduct possibleProduct = _products.FirstOrDefault(p => p.Id == obj);
+            IProduct possibleProduct = Products.FirstOrDefault(p => p.Id == idProduct);
             if (possibleProduct != null)
             {
-                _products.Remove(possibleProduct);
-                RefreshProducts();
+                UpdateProductsCollection(() => Products.Remove(possibleProduct));
             }
         }
+        private void UpdateProductsCollection(Action updateAction)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                updateAction();
+                RefreshProducts();
+            });
+        }
+        private void CheckIfProductIsValid()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                MessageBox.Show("Le nom du produit ne peut pas être vide.");
+                return;
+            }
 
+            if (Quantity <= 0)
+            {
+                MessageBox.Show("La quantité doit être supérieure à zéro.");
+                return;
+            }
+
+            if (UnitPrice <= 0)
+            {
+                MessageBox.Show("Le prix unitaire doit être supérieur à zéro.");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(SelectedType))
+            {
+                MessageBox.Show("Veuillez sélectionner un type de produit.");
+                return;
+            }
+        }
         private async Task AddProduct()
         {
-
-            IProduct p = SelectedType == "Vegetable" ? new Vegetable(Name, Quantity, UnitPrice) : new Fruit(Name, Quantity, UnitPrice);
-            await Task.Run(() => _productStore.AddProduct(p));
-
-        }
-        private void RefreshProducts()
-        {
-            /*   _productList.Items.Clear();
-               foreach (IProduct product in _products)
-               {
-                   _productList.Items.Add(product);
-               }*/
-        }
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            List<ProductAvailabilityChecker> checkers = new List<ProductAvailabilityChecker>();
-            List<Thread> t = new List<Thread>();
-            foreach (IProduct p in _products)
+            CheckIfProductIsValid();
+            IProduct product;
+            if (SelectedType == "Vegetable")
             {
-                ProductAvailabilityChecker productAvailabilityChecker = new ProductAvailabilityChecker(p);
-                checkers.Add(productAvailabilityChecker);
-                Thread thread = new Thread(productAvailabilityChecker.CheckIfAvailable);
-                t.Add(thread);
-                thread.Start();
+                product = new Vegetable(Name, Quantity, UnitPrice);
             }
-            foreach (Thread thread in t)
+            else if (SelectedType == "Fruit")
             {
-                thread.Join();
-            }
-
-            StringBuilder sb = new StringBuilder();
-            bool anyError = false;
-            foreach (ProductAvailabilityChecker checker in checkers)
-            {
-                if (!checker.Result)
-                {
-                    anyError = true;
-                    sb.AppendLine("The product " + checker.Product.Name + " is not available");
-                }
-            }
-            if (!anyError)
-            {
-                //     MessageBox.Show(this, "Everything is available.");
+                product = new Fruit(Name, Quantity, UnitPrice);
             }
             else
             {
-                //     MessageBox.Show(this, sb.ToString());
+                MessageBox.Show("Type de produit non valide.");
+                return;
+            }
+            await Task.Run(() => _productStore.AddProduct(product));
+
+        }
+
+        private void RefreshProducts()
+        {
+            var products = _productStore.GetProducts().ToList();
+            Products.Clear();
+            foreach (var product in products)
+            {
+                Products.Add(product);
+            }
+        }
+        //TODO
+        private async Task CheckAvailability()
+        {
+            IsCheckingAvailability = false;
+            bool anyError = false;
+            var sb = new StringBuilder();
+            try
+            {
+                var checkers = Products.Select(p => new ProductAvailabilityChecker(p)).ToList();
+                var tasks = checkers.Select(async c => await Task.Run(() => c.CheckIfAvailable())).ToList();
+                await Task.WhenAll(tasks);
+
+                foreach (var checker in checkers)
+                {
+                    if (!checker.Result)
+                    {
+                        anyError = true;
+                        sb.AppendLine($"The product {checker.Product.Name} is not available");
+                    }
+                }
+            }
+            finally
+            {
+                IsCheckingAvailability = true;
+            }
+
+            if (!anyError)
+            {
+                MessageBox.Show("Everything is available.");
+            }
+            else
+            {
+                MessageBox.Show(sb.ToString());
             }
         }
         private void OnPropertyChanged(string propertyName)
